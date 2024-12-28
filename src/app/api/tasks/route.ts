8eb7b7fca00ec40fd9f2939c1c5c46ef8b0b59fe
matheus <matheus.felipe.19rt@gmail.com>
2024-12-28 12:45:08 -0300

    feat: implement PUT & DELETE methods and fetching methods for task management

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -35,14 +35,33 @@ export async function POST(req: NextRequest) {
 	return NextResponse.json(task, { status: 201 })
 }
 
-// // PUT method for updating a user task
-// export async function PUT(req: NextRequest) {
-// 	const { error, session, response } = await getSessionOrUnauthorized()
-// 	if (error) return response
-// }
+// PUT method for updating a user task
+export async function PUT(req: NextRequest) {
+	const { error, response } = await getSessionOrUnauthorized()
+	if (error) return response
 
-// // DELETE method for deleting a user task
-// export async function DELETE(req: NextRequest) {
-// 	const { error, session, response } = await getSessionOrUnauthorized()
-// 	if (error) return response
-// }
+	const { id, title, content, dueDate, priority, status } = await req.json()
+
+	const existingTask = await db.task.findFirst({ where: { id } })
+	if (!existingTask) return NextResponse.json({ error: "Task not found" }, { status: 404 })
+
+	const updatedTask = await db.task.update({ where: { id }, data: { title, content, dueDate, priority, status } })
+
+	return NextResponse.json(updatedTask, { status: 200 })
+}
+
+// DELETE method for deleting a user task
+export async function DELETE(req: NextRequest) {
+	const { error, response } = await getSessionOrUnauthorized()
+	if (error) return response
+
+	const id = req.nextUrl.searchParams.get("id")
+	if (!id) return NextResponse.json({ error: "Invalid input" }, { status: 400 })
+
+	const existingTask = await db.task.findFirst({ where: { id } })
+	if (!existingTask) return NextResponse.json({ error: "Task not found" }, { status: 404 })
+
+	await db.task.delete({ where: { id } })
+
+	return NextResponse.json({ id })
+}
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,4 +1,33 @@
+import { Task } from "@prisma/client"
+
+// Get user tasks
 export const getTasks = async () => {
 	const res = await fetch("/api/tasks", { method: "GET" })
+	return res.json()
+}
+
+// Create a new task
+export const createTask = async (task: Task) => {
+	const res = await fetch("/api/tasks", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(task)
+	})
+	return res.json()
+}
+
+// Update an existing task
+export const updateTask = async (task: Task) => {
+	const res = await fetch(`/api/tasks/${task.id}`, {
+		method: "PUT",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(task)
+	})
+	return res.json()
+}
+
+// Delete an existing task
+export const deleteTask = async (id: number) => {
+	const res = await fetch(`/api/tasks/${id}`, { method: "DELETE" })
 	return res.json()
 }
